02ee926b182f879a7f03ac3424b19f64a2f49592
vitorbruno404 <vitor.bruno@vitorbruno.com>
2025-02-01 13:26:10 +0100

    fixing mistakes

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['api.simli.ai', 'app.simli.com'],
+  },
+}
+
+module.exports = nextConfig 
